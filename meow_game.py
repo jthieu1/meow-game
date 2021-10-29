f8b38d8f050e1884de304d09e896ce228d4fc266
Jenny Thieu <thieuj@oregonstate.edu>
2021-10-28 23:37:15 -0400

    debugged ravioli score registering issue yay!

--- meow_game.py
+++ meow_game.py
@@ -50,6 +50,7 @@ def draw_text(text, font, text_clr, x_coord, y_coord):
 
 
 class Cat(pygame.sprite.Sprite):  # class for our player sprite, Thickems the Cat
+    """Thickems the Cat player sprite class"""
     def __init__(self, x_coord, y_coord):  # construct x and y coordinates
         pygame.sprite.Sprite.__init__(self)
         self.images = []  # images used are sorted in a list
